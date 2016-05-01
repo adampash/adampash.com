15309777b9f3518c08d303c5ee950dbcdfffeeff
Adam Pash <adam.pash@gmail.com>
2016-05-01 11:09:15 -0400

    added keybase.txt

--- lib/adam_pash/endpoint.ex
+++ lib/adam_pash/endpoint.ex
@@ -10,7 +10,7 @@ defmodule AdamPash.Endpoint do
   plug Plug.Static,
     at: "/", from: :adam_pash, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt
-            lifeinvader assets javascripts)
+            assets javascripts keybase.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
